66d39d4e5e72941dc42cedb07606cda283705592
AnikaSood28 <157402781+AnikaSood28@users.noreply.github.com>
2024-10-22 00:02:13 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,13 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config();
-
-const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); 
+const __dirname = path.resolve();
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
